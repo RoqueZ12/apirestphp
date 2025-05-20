35656f8a45233750587613d21217f26217ab07fa
santino <ing.roquezegarra@gmail.com>
2025-05-20 15:34:28 -0500

    aadadada

--- routes/loginGoogle.php
+++ routes/loginGoogle.php
@@ -16,9 +16,12 @@ if (in_array($origin, $allowed_origins)) {
 }
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit();
+    header("Access-Control-Allow-Origin: https://miniecommerce-dun.vercel.app");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    exit(0);
 }
+
 
 // Requiere archivos necesarios
 require_once __DIR__ . '/../database/db.php';
